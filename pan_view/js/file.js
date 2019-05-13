d5b4c44469d8280cdcb62e810ba2a7d0023fad14
jkkwjk <465640448@qq.com>
2019-05-13 11:24:59 +0800

    主页面布局完成

--- pan_view/js/file.js
+++ pan_view/js/file.js
@@ -1,3 +1,19 @@
-function select_all(){
-    
-}
+$(document).ready(function(){
+    var timer = null;
+    $("#user_name_group").mouseenter(function(){
+        var elemt = $("#user_name_animation_nomal");
+        clearTimeout(timer);
+        elemt.stop(true);
+        elemt.css('display','inline');
+        elemt.animate({'opacity':'1'},{queue:false,duration:200});
+        elemt.animate({'margin-top':'0px'},{queue:false,duration:200});
+
+    });
+    $("#user_name_group").mouseleave(function(){
+        var elemt = $("#user_name_animation_nomal");
+        elemt.stop(true);
+        elemt.animate({'margin-top':'-10px'},{queue:false,duration:200});
+        elemt.animate({'opacity':'0'},{queue:false,duration:200});
+        timer = setTimeout("$(\"#user_name_animation_nomal\").css('display','none')",200);
+    });
+});
