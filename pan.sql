aa9fdda5c365eb41e0a431b374a8796cb641810b
jkkwjk <465640448@qq.com>
2019-05-28 12:23:11 +0800

    增加sql文件

--- pan.sql
+++ pan.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 23/05/2019 13:09:19
+ Date: 28/05/2019 12:22:56
 */
 
 SET NAMES utf8mb4;
@@ -27,13 +27,20 @@ CREATE TABLE `file`  (
   `file_size` varchar(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '文件大小 单位B',
   `folder_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '文件在服务器上的位置',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of file
 -- ----------------------------
-INSERT INTO `file` VALUES (1, '5A105E8B9D40E1329780D62EA2265D8A', '5', '1');
-INSERT INTO `file` VALUES (2, '7E9A5A68E62486CC1D9295C6C304F08C', '14', '1');
+INSERT INTO `file` VALUES (17, 'aa2e45c21935e3a2d2b7efee6fecb621', '1395742', '0');
+INSERT INTO `file` VALUES (18, '14d37a3b07110b1737589c82f6f810d8', '152521', '0');
+INSERT INTO `file` VALUES (19, 'dd9572061eab9cc0042459894a5ca14a', '618496', '0');
+INSERT INTO `file` VALUES (20, '0c3745f5c550f2d9a3b3322f1b988868', '297984', '0');
+INSERT INTO `file` VALUES (21, '4ed36b5b811e5484027db09acc818cd2', '7131', '0');
+INSERT INTO `file` VALUES (22, 'bb4c012fe71e2bac7664ff9d976bdf94', '6359869', '0');
+INSERT INTO `file` VALUES (26, '3b8362c679544d1b780699ba7398ef2c', '890556', '0');
+INSERT INTO `file` VALUES (27, 'e8d709784feacac6d3a4d8e342389129', '24', '0');
+INSERT INTO `file` VALUES (28, 'a0fb845d0b3e56c0f87ab4c52e81be24', '23188594', '0');
 
 -- ----------------------------
 -- Table structure for folder
@@ -43,18 +50,19 @@ CREATE TABLE `folder`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文件夹ID',
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '文件夹名字',
   `user_id` int(11) NOT NULL COMMENT '所属用户ID',
-  `folder_time` timestamp(0) NOT NULL COMMENT '修改文件夹内容的时间',
-  `p_folder_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '上级文件夹ID 顶部为0',
+  `folder_time` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '修改文件夹内容的时间',
+  `p_folder_id` int(11) NOT NULL COMMENT '上级文件夹ID 顶部为0',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `用户ID`(`user_id`) USING BTREE,
+  INDEX `p_folder_id`(`p_folder_id`) USING BTREE,
   CONSTRAINT `用户ID` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of folder
 -- ----------------------------
-INSERT INTO `folder` VALUES (1, '一级文件夹', 1004, '2019-05-07 00:00:00', '0');
-INSERT INTO `folder` VALUES (4, 'SDF', 1004, '2019-05-22 22:06:22', '0');
+INSERT INTO `folder` VALUES (18, 'terestt', 1004, '1558925286', 0);
+INSERT INTO `folder` VALUES (19, '你好啊', 1004, '1558926740', 0);
 
 -- ----------------------------
 -- Table structure for permission
@@ -82,7 +90,7 @@ CREATE TABLE `share_base`  (
   `type` int(1) NOT NULL COMMENT 'folder为0 file为1',
   `link_id` int(11) NOT NULL COMMENT '链接到 share_file 或 share_folder 表的ID',
   `pwd` varchar(4) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '分享密码',
-  `limit_time` timestamp(0) NOT NULL COMMENT '到期时间',
+  `limit_time` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '到期时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
@@ -116,12 +124,13 @@ CREATE TABLE `user`  (
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '用户密码 两遍md5',
   `permission_id` int(1) NOT NULL DEFAULT 0 COMMENT '权限ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1004 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1006 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1004, 'admin', '0c909a141f1f2c0a1cb602b0b2d7d050', 1);
+INSERT INTO `user` VALUES (1006, 'test', 'a60bf8ccb8bb374d12aa50c977c9dc37', 0);
 
 -- ----------------------------
 -- Table structure for user_file
@@ -132,18 +141,17 @@ CREATE TABLE `user_file`  (
   `user_id` int(11) NOT NULL COMMENT '用户ID',
   `file_id` int(11) NOT NULL COMMENT '文件ID',
   `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '文件名,相同文件可能有不同文件名',
-  `file_time` timestamp(0) NOT NULL COMMENT '上传文件的时间',
+  `file_time` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '上传文件的时间',
   `folder_id` int(11) NOT NULL COMMENT '文件所处用户文件夹ID',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `userID`(`user_id`) USING BTREE,
   CONSTRAINT `userID` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 32 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user_file
 -- ----------------------------
-INSERT INTO `user_file` VALUES (4, 1004, 1, 'adminname', '2019-04-07 22:04:16', 0);
-INSERT INTO `user_file` VALUES (5, 1004, 1, 'flename2', '2019-05-22 21:52:45', 0);
+INSERT INTO `user_file` VALUES (28, 1004, 26, '计划.e', '1558776083', 0);
 
 -- ----------------------------
 -- Table structure for user_info
@@ -161,5 +169,16 @@ CREATE TABLE `user_info`  (
   PRIMARY KEY (`userid`) USING BTREE,
   CONSTRAINT `user主表` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+INSERT INTO `user_info` VALUES (1006, 'test', NULL, NULL, NULL, NULL, NULL, '10737418240');
+
+-- ----------------------------
+-- View structure for file_num_in_system
+-- ----------------------------
+DROP VIEW IF EXISTS `file_num_in_system`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `file_num_in_system` AS select `file`.`folder_path` AS `path`,count(`file`.`folder_path`) AS `num` from `file` group by `file`.`folder_path`;
 
 SET FOREIGN_KEY_CHECKS = 1;
